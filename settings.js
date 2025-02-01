22bdaacd5df8546145badacf52a8fb3506f504f4
AJSWRLD1 <167708025+AJSWRLD1@users.noreply.github.com>
2025-02-01 20:02:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'AJ',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "AJ's Bot",
+  botOwner: 'AJSWRLD', // Your name
+  ownerNumber: '2349028774813', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
